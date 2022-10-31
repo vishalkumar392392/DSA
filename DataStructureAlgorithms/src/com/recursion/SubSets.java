0177802c95e7b9a0c8d8ee64bc65882bcc38a2d2
Vishal <apple@APPLEs-MacBook-Air.local>
2022-11-01 02:48:47 +0530

    changes

--- DataStructureAlgorithms/src/com/recursion/SubSets.java
+++ DataStructureAlgorithms/src/com/recursion/SubSets.java
@@ -4,6 +4,8 @@ public class SubSets {
 
 	public static void main(String[] args) {
 		subSets("hetvi", "", 0);
+		System.out.println("==========================================");
+		subSetsApproach("hetvi", "", "hetvi".length());
 	}
 
 	public static void subSets(String s, String curr, int i) {
@@ -17,5 +19,18 @@ public class SubSets {
 		subSets(s, curr + s.charAt(i), i + 1);
 
 	}
+
+	public static void subSetsApproach(String s, String curr, int i) {
+
+		if (i <= 0) {
+			System.out.println(curr);
+			return;
+		}
+
+		subSetsApproach(s, curr, i - 1);
+		subSetsApproach(s, s.charAt(i - 1) + curr, i - 1);
+
+	}
+
 
 }
